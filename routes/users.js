6c10c0ae4bcf04cd82743a6bf332e7d3bebb5659
migigim <fm.rodriguezg@hotmail.com>
2021-11-20 00:02:27 -0600

    🔧 Updating routes directory

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const passport = require('passport');
+const express = require('express');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const router = express.Router();
+const {
+  login, register, createAdmin, deleteusuario, getAllUsuarioss,
+} = require('../controllers/Usuarios/UsuariosController');
+
+const options = { session: false };
+
+router.post('/register', register);
+router.post('/login', passport.authenticate('local', options), login);
+
+router.get('/current', passport.authenticate('bearer', options), getCurrentUsuarios);
+router.get('/', passport.authenticate('bearer', options), getAllUsuarioss);
+
+router.delete('/delete-user', passport.authenticate('bearer', options), deleteUser);
 
 module.exports = router;
